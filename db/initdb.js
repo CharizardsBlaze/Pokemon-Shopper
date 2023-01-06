c572cb6932380ed8d21d2e2641ef0a1d20e44ce6
cusmoritz <cus.moritz@gmail.com>
2023-01-06 16:55:17 -0700

    feat(getAllProducts): created SELECT statement for fetching products from database

--- db/initdb.js
+++ db/initdb.js
@@ -51,6 +51,18 @@ const createTables = async() => {
     }
 }
 
+const fetchAllProducts = async() => {
+    try {
+        const {rows: allProducts} = await client.query(`
+        SELECT * FROM products
+        ;
+        `);
+        return allProducts;
+    } catch (error) {
+        console.log('there was an error fetchingAllProducts from the database: ', error);
+        throw error;
+    }
+}
 
 module.exports = {
     dropTables, 

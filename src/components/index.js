3d0ec6c2b2047acbb7e203d56de0fd5dc102eec8
Pierce Babineaux <propurtyschannel@gmail.com>
2023-01-06 22:38:59 -0600

    feat(component): add file structure & starter code

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+export { default as HomePage } from "./Home";
+export { default as Cards } from "./Cards";
+export { default as CardDetail } from "./CardDetail";
+export { default as AccountDetails } from "./AccountDetails";
+export { default as AccountForm } from "./AccountForm";
+export { default as Cart } from "./Cart";
+
+// REVIEW - Potential components.
+// export { default as CardItem } from "./CardItem";
+// export { default as NavBar } from "./NavBar";
+// export { default as Header } from "./Header";
+// export { default as Footer } from "./Footer";

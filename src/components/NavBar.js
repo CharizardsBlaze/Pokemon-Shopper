93b29720017aaa1b73eb2d42ac30f638b9c087a0
Jaron <jaronchretien@yahoo.com>
2023-01-14 11:26:39 -0600

    feat(addToCard) adds item to cart

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import cart from './images/cart.png'
 // Will likely want useNavigate for logout.
 
 // TODO - This component is not needed if we put a condensed NavBar inside App.js instead.
@@ -8,12 +9,15 @@ const NavBar = () => {
   return (
     <nav>
       <div className='ui secondary pointing menu'>
-        <Link to='/' className='item'>
+        <NavLink to='/' className='item'>
           Home
-        </Link>
-        <Link to='/cards' className='item'>
+        </NavLink>
+        <NavLink to='/cards' className='item'>
           Cards
-        </Link>
+        </NavLink>
+        <NavLink to='/cart' className='item float right'>
+        <img src={cart} className="ui large shopping cart icon"/>
+        </NavLink>
       </div>
     </nav>
   );

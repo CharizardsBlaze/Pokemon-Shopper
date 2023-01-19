aae856b9c8b12ab2bb302dc94af2962f92c4b6f0
cusmoritz <cus.moritz@gmail.com>
2023-01-18 19:53:07 -0700

    feat: beginnings of a footer. needs an about page, if we want one.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {
   Register,
   Login,
   SearchBar,
+  Footer,
 } from "./components";
 
 // TODO - Refactor into needed components, props to pass to them, and routes.
@@ -76,6 +77,7 @@ const App = () => {
         <Route path="/login" element={<Login setToken={setToken} />}/>
         <Route className='item' path='/cart' element={<Cart user={user} token={token}/>} />
       </Routes>
+      <Footer />
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -8,9 +8,10 @@ export {default as SearchBar } from './SearchBar'
 export { default as Cards } from "./Cards";
 // export { default as CardItem } from "./CardItem";
 export { default as CardDetail } from "./CardDetail";
+export { default as Footer } from "./Footer";
 
 // export { default as AccountDetails } from "./AccountDetails";
 // export { default as AccountForm } from "./AccountForm";
 
 // export { default as Header } from "./Header";
-// export { default as Footer } from "./Footer";
+

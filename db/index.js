de34d538a5a34568a4719c46ede322d8ce20b111
ReleventGull <109637509+ReleventGull@users.noreply.github.com>
2023-01-31 19:23:36 -0600

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const {Client} = require('pg')
 
-const { DATABASE_URL = 'postgres://localhost:5432/todo-dev' } = process.env;
+const { DATABASE_URL = 'postgres://localhost:5432/pokemon-shopper' } = process.env;
 const client = new Client({
     connectionString: DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : undefined,
